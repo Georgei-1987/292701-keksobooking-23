2c3c2dee24b199e215c6cf5235b473eb1be6731b
Георгий <george_bpt@mail.ru>
2021-06-04 18:38:48 +0300

    Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -1,49 +1,46 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-// Пример использования функции:
 
-// имя_функции(от, до); // Результат: целое число из диапазона "от...до"
+//Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-// Учтите, что диапазон может быть только положительный, включая ноль.
-// А также придумайте, как функция должна вести себя, если передать значение «до» меньшее,
-// чем значение «от», или равное ему.
-
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+/**
+ * Возвращает случайное число из указанного диапазона (min и max входят в диапазон).
+ *
+ * @param {number} min Нижний предел.
+ * @param {number} max Верхний предел.
+ */
 
 function getRandomIntInclusive(min, max) {
   if (min <0 || max < 0 || min >= max) {
-    // console.log('В диапазоне указаны неверные числа.\nВведите пожалуйста правильные числа.');
-    return '';
+
+    return null;
   }
+
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 getRandomIntInclusive(2, 10);
 
+//Источник: https://question-it.com/questions/1483098/generatsija-sluchajnogo-chisla-mezhdu-dvumja-chislami-v-javascript
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-// Будет использоваться для генерации временных географических координат в следующем задании.
-// Пример использования функции:
+/**
+ * Возвращает случайное число с плавающей точкой с "указанным количеством знаков после запятой"
+ * (min и max входят в диапазон).
+ *
+ * @param {number} min Нижний предел.
+ * @param {number} max Верхний предел.
+ */
 
-// имя_функции(от, до, количество_знаков_после_запятой);
-// Результат: число с плавающей точкой из диапазона "от...до" с указанным
-// "количеством знаков после запятой"
-
-// Учтите, что диапазон может быть только положительный, включая ноль.
-// А также придумайте, как функция должна вести себя, если передать значение «до» меньшее,
-// чем значение «от», или равное ему. Не забудьте, что в случае с дробными числами диапазон
-// может быть в десятых, сотых, тысячных и т. д. долях.
-// Например, 1.1, 1.2 — корректный диапазон.
-
-// https://question-it.com/questions/1483098/generatsija-sluchajnogo-chisla-mezhdu-dvumja-chislami-v-javascript
-
-function randomFloatFromInterval (min, max, fractionDigits) {
+function getRandomFloatInclusive(min, max, fractionDigits) {
   if (min < 0 || max < 0 || min >= max || fractionDigits <= 0) {
-    // console.log('В диапазоне указаны неверные числа.\nВведите пожалуйста правильные числа.');
-    return '';
+
+    return null;
   }
+
   const fractionMultiplier = Math.pow(10, fractionDigits);
+
   return Math.round((Math.random() * (max - min) + min) * fractionMultiplier) / fractionMultiplier;
 }
-randomFloatFromInterval(1, 10, 1);
+
+getRandomFloatInclusive(1, 10, 1);
